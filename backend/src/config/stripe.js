99e856a9daa817f5c22e4b38157e2c1a5b1bf27a
whitekaden75 <whitekaden75@gmail.com>
2026-04-15 16:09:45 -0600

    stripe log

--- backend/src/config/stripe.js
+++ backend/src/config/stripe.js
@@ -1,6 +1,9 @@
 import Stripe from "stripe";
 import { env } from "./env.js";
 
+console.log("Stripe key exists:", !!env.stripeSecretKey);
+console.log("Stripe key value:", env.stripeSecretKey);
+
 export const stripe = new Stripe(env.stripeSecretKey, {
   apiVersion: "2025-03-31.basil",
 });
